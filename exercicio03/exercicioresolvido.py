9c62a4d84b63e5f6257ef512eac1ca1b2aedb8ec
karineyasmin <karine.yasmin@outlook.com>
2024-08-03 14:08:41 -0300

    adicionado exercicio 03

---
+++ exercicio03/exercicioresolvido.py
@@ -0,0 +1,13 @@
+# 3. Faça um programa que peça uma nota, entre zero e dez.
+# Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido
+
+while True:
+    try:
+        nota = int(input("Por favor, digite uma nota (0-10) -> "))
+        if 0 <= nota <= 10:
+            print(f"Nota: {nota}")
+            break
+        else:
+            print("Valor inválido! A nota deve estar entre 0 e 10.")
+    except ValueError:
+        print("Entrada inválida! A nota deve ser um número inteiro.")
