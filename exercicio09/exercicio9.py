3ab287e9d0a3b0fb76202d0734f2cabf519b5386
Sofia Mayumi <mayuminakamaru1002@gmail.com>
2024-08-02 22:58:27 -0300

    enviando as pastas com os arquivos exericico9.py e exercicio10.py na pasta Desafio2_SquadBerthaLutz

---
+++ exercicio09/exercicio9.py
@@ -0,0 +1,22 @@
+contagem_pares =0
+contagem_impar= 0
+
+
+while True:
+  numero = int(input('Digite um numero(0 para encerrar)'))
+  if numero == 0:
+   break
+  if numero<0:
+    print("Digite um numero positivo")
+    continue
+   
+if numero %2 ==0:
+    contagem_pares +=1
+
+else:
+   contagem_impar +=1
+
+
+
+print(f'A quantidade de numeros pares sao: {contagem_pares}')
+print(f'A quantidade de numeros Impares sao: {contagem_impar}')
---
+++ exercicio10/exercicio10.py
@@ -0,0 +1,9 @@
+numero01 = int(input('Digite o primeiro numero '))
+numero02 = int(input('Digite o segundo numero '))
+numero03 = int(input('Digite o terceiro numero '))
+
+numeros = [numero01,numero02,numero03]
+
+numeroOrdem = sorted(numeros)
+
+print(f'Os numeros em ordem crescente sao  {numeroOrdem}')
